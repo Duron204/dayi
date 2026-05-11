a089bfcca0653ad7671183bfe84abb6a27d6f005
Duron204 <2718224631@qq.com>
2026-05-11 17:09:25 +0800

    鏇存柊浠ｇ爜

---
+++ python学习/010常用数据结构之字符串.py
@@ -0,0 +1,15 @@
+# 字符串的定义
+s1 = 'hello, world!'
+s2 = "你好，世界！❤️"
+s3 = '''hello,
+wonderful
+world!'''
+print(s1)
+print(s2)
+print(s3)
+
+# 转义字符
+s1 = '\'hello, world!\''
+s2 = '\\hello, world!\\'
+print(s1)
+print(s2)
