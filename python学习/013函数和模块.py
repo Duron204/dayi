db9f515905c9fe1220bb644a3f1b9a89aba6ac38
Duron204 <2718224631@qq.com>
2026-05-15 14:45:56 +0800

    更新代码: 1

--- python学习/013函数和模块.py
+++ python学习/013函数和模块.py
@@ -1,18 +1,43 @@
+# 定义函数
 # 输入m和n，计算组合数C(m,n)的值
+# 通过关键字def定义求阶乘的函数
+# 自变量（参数）num是一个非负整数
+# 因变量（返回值）是num的阶乘
+def fac(num):
+    result = 1
+    for n in range(2, num + 1):
+        result *= n
+    return result
+
 
 m = int(input('m = '))
 n = int(input('n = '))
-# 计算m的阶乘
-fm = 1
-for num in range(1, m + 1):
-    fm *= num
-# 计算n的阶乘
-fn = 1
-for num in range(1, n + 1):
-    fn *= num
-# 计算m-n的阶乘
-fk = 1
-for num in range(1, m - n + 1):
-    fk *= num
-# 计算C(M,N)的值
-print(fm // fn // fk)
+# 计算阶乘的时候不需要写重复的代码而是直接调用函数
+# 调用函数的语法是在函数名后面跟上圆括号并传入参数
+print(fac(m) // fac(n) // fac(m - n))
+
+# 输入m和n，计算组合数C(m,n)的值
+from math import factorial
+
+m = int(input('m = '))
+n = int(input('n = '))
+print(factorial(m) // factorial(n) // factorial(m - n))
+
+# 输入m和n，计算组合数C(m,n)的值
+from math import factorial as f
+
+m = int(input('m = '))
+n = int(input('n = '))
+print(f(m) // f(n) // f(m - n))
+
+# 函数的参数
+# 位置参数和关键字参数
+def make_judgement(a, b, c):
+    """判断三条边的长度能否构成三角形"""
+    return a + b > c and b + c > a and a + c > b
+# 上面make_judgement函数有三个参数，这种参数叫做位置参数，在调用函数时通常按照从左到右的顺序依次传入，而且传入参数的数量必须和定义函数时参数的数量相同，如下所示。
+print(make_judgement(1, 2, 3))  # False
+print(make_judgement(4, 5, 6))  # True
+print(make_judgement(b=2, c=3, a=1))  # False
+print(make_judgement(c=6, b=4, a=5))  # True
+
